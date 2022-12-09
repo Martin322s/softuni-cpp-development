174acd2bc4c7e2220c7c0621be519a2d23bee664
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-09 18:41:44 +0200

    append to body

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,23 +10,22 @@ export default class Application extends EventEmitter {
 
     constructor() {
         super();
-        this.loading = document.querySelector('.progress');
-        this.startLoadig(true);
-        this.create();
-
+        this._loading = document.querySelector('.progress');
+        this._startLoadig(true);
+        this._create();
         this.emit(Application.events.READY);
     }
 
-    async load() {
+    async _load() {
         const res = await fetch('https://swapi.boom.dev/api/planets');
         const planets = await res.json();
         return planets.results;
     }
 
-    create() {
+    _create() {
         this._load()
-            .then(result => {
-                result.forEach(element => {
+            .then(planets => {
+                planets.forEach(element => {
                     const box = document.createElement("div");
                     box.classList.add("box");
                     box.innerHTML = this._render({
@@ -35,12 +34,12 @@ export default class Application extends EventEmitter {
                         population: element.population,
                     });
                     this._stopLoadig(false);
-                    document.body.querySelector(".main").appendChild(box);
+                    document.body.appendChild(box);
                 });
             });
     }
 
-    startLoadig(isLoading) {
+    _startLoadig(isLoading) {
         if (isLoading) {
             this._loading.style.display = 'block';
         } else {
@@ -48,7 +47,7 @@ export default class Application extends EventEmitter {
         }
     }
 
-    stopLoadig(isLoading) {
+    _stopLoadig(isLoading) {
         if (isLoading) {
             this._loading.style.display = 'block';
         } else {
@@ -56,7 +55,7 @@ export default class Application extends EventEmitter {
         }
     }
 
-    render({ name, terrain, population }) {
+    _render({ name, terrain, population }) {
         return `
             <article class="media">
                 <div class="media-left">
@@ -68,12 +67,11 @@ export default class Application extends EventEmitter {
                         <div class="content">
                             <h4>${name}</h4>
                             <p>
-                                <span class="tag">${terrain}</span> 
-                                <span class="tag">${population}</span>
+                                <span class="tag">${terrain}</span> <span class="tag">${population}</span>
                                 <br>
                             </p>
                         </div>
                     </div>
-            </article>`;
+                </article>`;
     }
 }
