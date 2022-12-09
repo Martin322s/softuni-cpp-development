d79f9bbadc13362a6aa7cf77fdff550c4dc3861e
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-09 18:21:42 +0200

    add start stop loading

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,7 +11,7 @@ export default class Application extends EventEmitter {
     constructor() {
         super();
         this._loading = document.querySelector('.progress');
-        
+        this._startLoadig(true);
         this._create();
 
         this.emit(Application.events.READY);
@@ -24,48 +24,57 @@ export default class Application extends EventEmitter {
     }
 
     _create() {
-        this._load()
-            .then(result => {
-                result.forEach(element => {
-                    const box = document.createElement("div");
-                    box.classList.add("box");
-                    box.innerHTML = this._render({
-                        name: element.name,
-                        terrain: element.terrain,
-                        population: element.population,
+        setTimeout(() => {
+            this._load()
+                .then(result => {
+                    result.forEach(element => {
+                        const box = document.createElement("div");
+                        box.classList.add("box");
+                        box.innerHTML = this._render({
+                            name: element.name,
+                            terrain: element.terrain,
+                            population: element.population,
+                        });
+                        this._stopLoadig(false);
+                        document.body.querySelector(".main").appendChild(box);
                     });
-                    this._loading.style.display = 'none';
-                    document.body.querySelector(".main").appendChild(box);
                 });
-            });
+        }, 3000)
     }
 
-    _startLoadig() {
-
+    _startLoadig(isLoading) {
+        if (isLoading) {
+            this._loading.style.display = 'block';
+        } else {
+            this._loading.style.display = 'none';
+        }
     }
 
-    _stopLoadig() {
-
+    _stopLoadig(isLoading) {
+        if (isLoading) {
+            this._loading.style.display = 'block';
+        } else {
+            this._loading.style.display = 'none';
+        }
     }
 
     _render({ name, terrain, population }) {
         return `
-<article class="media">
-  <div class="media-left">
-    <figure class="image is-64x64">
-      <img src="${image}" alt="planet">
-    </figure>
-  </div>
-  <div class="media-content">
-    <div class="content">
-    <h4>${name}</h4>
-      <p>
-        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
-        <br>
-      </p>
+    <article class="media">
+    <div class="media-left">
+        <figure class="image is-64x64">
+            <img src="${image}" alt="planet">
+        </figure>
     </div>
-  </div>
-</article>
-    `;
+        <div class="media-content">
+            <div class="content">
+                <h4>${name}</h4>
+                <p>
+                    <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+                <br>
+                </p>
+            </div>
+        </div>
+    </article>`;
     }
 }
