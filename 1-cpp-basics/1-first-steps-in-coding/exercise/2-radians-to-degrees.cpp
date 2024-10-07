009ef98b648a8522f8f932d67b063192d81dd01d
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-10-07 17:56:52 +0300

    2-rad-to-deg

---
+++ 1-cpp-basics/1-first-steps-in-coding/exercise/2-radians-to-degrees.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	double radians, degrees;
+	
+	cin >> radians;
+	
+	degrees = radians * (180 / 3.14);
+	
+	cout << round(degrees) << endl;
+}
