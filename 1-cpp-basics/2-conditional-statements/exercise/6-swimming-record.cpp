46b3975d94db756aa5b0fb259c5d6102c7fc8f0b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-11-04 12:30:23 +0200

    1-day-of-week

--- 1-cpp-basics/2-conditional-statements/exercise/6-swimming-record.cpp
+++ 1-cpp-basics/2-conditional-statements/exercise/6-swimming-record.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
+int main() {
+    double worldRecord, distance, timePerMeter;
+    cin >> worldRecord;
+    cin >> distance;
+    cin >> timePerMeter;
+
+    double swimTime = distance * timePerMeter;
+    double delay = floor(distance / 15) * 12.5;
+    double totalTime = swimTime + delay;
+    
+    cout << fixed << setprecision(2);
+    
+    if (totalTime < worldRecord) {
+        cout << "Yes, he succeeded! The new world record is " << totalTime << " seconds." << endl;
+    } else {
+        cout << "No, he failed! He was " << totalTime - worldRecord << " seconds slower." << endl;
+    }
+
+    return 0;
+}
