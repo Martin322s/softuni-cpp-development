546109f6c8140796fe170769d41c92bda193cb8a
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-07-19 15:59:52 +0300

    book properties

---
+++ more-exercises/library-system.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+class Book {
+	public:
+		string title;
+		string author;
+		string isbn;
+		int year;
+		bool isBorrowed = false;
+
+		
+};
+
+int main()
+{
+
+	return 0;
+}
