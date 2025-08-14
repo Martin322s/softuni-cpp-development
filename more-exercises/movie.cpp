f860e5a7f31e97036800b0859278fe94a4e51e1c
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-08-14 15:19:56 +0300

    public constructor

--- more-exercises/movie.cpp
+++ more-exercises/movie.cpp
@@ -2,8 +2,17 @@
 #include <string>
 using namespace std;
 
-class Movie {
-    string title;
-    int year;
-    double rating;
+class Movie
+{
+	string title;
+	int year;
+	double rating;
+
+public:
+	Movie(string t, int y, double r)
+	{
+		title = t;
+		year = y;
+		rating = r;
+	}
 };
