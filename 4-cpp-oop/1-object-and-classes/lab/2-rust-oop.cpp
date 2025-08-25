e984e393cbd42b24d861379e4aeaad51f22e9a0f
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-08-25 14:52:26 +0300

    rust oop version

---
+++ 4-cpp-oop/1-object-and-classes/lab/2-rust-oop.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+const size_t DIMENSION = 10;
+
+const char RUST = '!';
+const char ALPAKA = '#';
+const char EMPTY = '.';
+const char TEMP_RUST = '?';
+
+class Board {
+	
+	char board[DIMENSION][DIMENSION];
+	
+	public:
+		Board(istream & istr) {
+			for(size_t row = 0; row < DIMENSION; row++) {
+				for(size_t col = 0; col < DIMENSION; col++) {
+					istr >> board[row][col];
+				}
+			}
+		}
+		
+		void setCell(int row, int col, char symbol) {
+			if (row < 0 || col < 0 || row >= DIMENSION || col >= DIMENSION) {
+				return;
+			} else {
+				board[row][col] = symbol;
+			}
+		}
+		
+		void setRust(int row, int col, char symbol) {
+
+            if (row < 0 || col < 0 || row >= DIMENSION || col >= DIMENSION)
+                return;
+            
+            if (board[row][col] == EMPTY)    
+                board[row][col] = symbol;
+        }
+        
+        void print(ostream & os) {
+            for(size_t row = 0; row < DIMENSION; row++) {
+                for(int col = 0; col < DIMENSION; col++)
+                    os << board[row][col]; 
+                os << endl;
+            }
+        }
+        
+        void putTempRust() {
+        	for(size_t row = 0; row < DIMENSION; row++) {
+				for(size_t col = 0; col < DIMENSION; col++) {
+					if (board[row][col] == RUST) {
+						setRust(row - 1, col, TEMP_RUST);
+						setRust(row + 1, col, TEMP_RUST);
+						setRust(row, col - 1, TEMP_RUST);
+						setRust(row, col + 1, TEMP_RUST);
+					}
+				}
+			}
+		}
+		
+		void fixTempRust() {
+			for(int row = 0; row < DIMENSION; row++)
+                for(int col = 0; col < DIMENSION; col++) 
+                    if (board[row][col] == TEMP_RUST)
+                        board[row][col] = RUST;
+		}
+};
+
+
+class Rust {
+	istream & is;
+	ostream & os;
+	
+	Board b;
+	size_t ticks;
+	
+	public:
+		Rust(istream & istr, ostream & ostr) :
+			is(istr),
+			os(ostr),
+			b(istr)
+		{
+			is >> ticks;	
+		}
+		
+		void process() {
+			for(; ticks; ticks--) {
+				b.putTempRust();
+				b.fixTempRust();
+			}
+			
+			b.print(os);
+		}
+};
+
+int main() {
+	Rust r(cin, cout);
+	
+	r.process();
+	
+	return 0;
+}
+
