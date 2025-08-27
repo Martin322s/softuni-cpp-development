c8b75e1f3cde9d106e9d829ae7ca6eee335fbf88
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-08-27 14:54:46 +0300

    enums demo

--- 4-cpp-oop/1-object-and-classes/resources/01.Objects-and-Classes-Demos/02.Enumerations.cpp
+++ 4-cpp-oop/1-object-and-classes/resources/01.Objects-and-Classes-Demos/02.Enumerations.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    enum planets {earth = 3, mars, jupiter, saturn, uranus, neptune};
+    enum planets { earth = 3, mars, jupiter, saturn, uranus, neptune };
 
     // We can do math on this enum, because it is just const int underneath
     cout << "earth (" << earth << ") is "
         << uranus - earth << " planets away from "
         << "uranus (" << uranus << ")" << endl;
 
-    enum class Planets {earth = 3, mars, jupiter, saturn, uranus, neptune};
+    enum class Planets { earth = 3, mars, jupiter, saturn, uranus, neptune };
 
     Planets planetEarth = Planets::earth;
     // the line below will cause a compilation error - just earth means the other enum's earth
-    //Planets planetEarthInvalid = earth;
+    // Planets planetEarthInvalid = earth;
 
     // You can also specify the type the enum class uses
     // NOTE: we can't do math on these without explicitly converting them to a numeric type
