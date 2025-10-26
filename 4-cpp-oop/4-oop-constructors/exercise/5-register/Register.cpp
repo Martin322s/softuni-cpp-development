4c16fb90b2dbd316243aa9b452ac40a5bedd85f4
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-10-26 18:45:52 +0200

    register cpp file

---
+++ 4-cpp-oop/4-oop-constructors/exercise/5-register/Register.cpp
@@ -0,0 +1,25 @@
+#include "Register.h"
+
+Register::Register(size_t numCompanies) : 
+	companiesArray(new Company[numCompanies]), numAdded(0) {}
+
+void Register::add(const Company& c) {
+	companiesArray[numAdded] = c;
+	numAdded++;
+}
+
+Company Register::get(int companyId) const {
+	for(size_t idx = 0; idx < numAdded; idx++) {
+		if (companiesArray[idx].getId() == companyId) {
+			return companiesArray[idx];
+		}
+	}
+	
+	return Company();
+}
+
+Register::~Register() {
+	if (companiesArray != nullptr) {
+		delete [] companiesArray;
+	}
+}
